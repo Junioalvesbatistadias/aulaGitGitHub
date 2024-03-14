c5fe250767422b7f757e9880c6100e2cce5b3672
Junioalvesbatistadias <142805123+Junioalvesbatistadias@users.noreply.github.com>
2024-03-14 10:06:19 -0300

    Update texte.js

--- texte.js
+++ texte.js
@@ -1,8 +1,7 @@
 function soma () {
 var n1, n2, soma;
-n1 = document.getElementById ("primeiroNumero").value;
+n1 = parseInt (document.getElementById ("primeiroNumero").value;)
 n2 = document.getElementById ("segundoNumero").value;
-n1 = parseInt (n1);
 n2 = parseInt (n2);
 soma = n1 + n2;
 }
